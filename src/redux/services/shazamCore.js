be9428029126b04ae6d51423ff0392c261f7f700
zuhair-credencesoft <zuhair@credencesoft.in>
2022-09-25 14:14:36 +0530

    API Implemented

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "fa97a08609mshf4efdc559491204p1c3838jsn070de7533a87"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable quotes */
+import { configureStore } from "@reduxjs/toolkit";
 
-import playerReducer from './features/playerSlice';
+import playerReducer from "./features/playerSlice";
+import { shazamCoreApi } from "./services/shazamCore";
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
